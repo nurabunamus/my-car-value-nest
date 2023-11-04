a8989eb41b4ca621dea635fc5ec5ece65f098bfb
Nur <noorin.sa.99@gmail.com>
2023-11-04 21:48:05 +0300

    Setting up user controller and validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Body } from '@nestjs/common';
+import { createUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: createUserDto) {
+    console.log(body);
+  }
+
+  //   @Get('/:id')
+  //   findUser() {}
+
+  //   @Get()
+  //   findAllUsers() {}
+
+  //   @Patch('/:id')
+  //   updateUser() {}
+
+  //   @Delete('/:id')
+  //   removeUser() {}
+}

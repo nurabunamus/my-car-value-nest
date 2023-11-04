3b0bac286e73bc3e2cf051dae2efc9aa0a6bc081
Nur <noorin.sa.99@gmail.com>
2023-11-04 22:00:40 +0300

    Create User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Patch, Delete, Body } from '@nestjs/common';
 import { createUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: createUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 
   //   @Get('/:id')
